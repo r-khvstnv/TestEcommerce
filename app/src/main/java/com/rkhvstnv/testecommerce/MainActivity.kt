5b751168b4c13c0cb32ad3ffe49234b5221eb16a
r-khvstnv <r.khvastunov@gmail.com>
2022-11-08 11:12:22 +0500

    Added bottom nav view

--- app/src/main/java/com/rkhvstnv/testecommerce/MainActivity.kt
+++ app/src/main/java/com/rkhvstnv/testecommerce/MainActivity.kt
@@ -2,10 +2,29 @@ package com.rkhvstnv.testecommerce
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rkhvstnv.testecommerce.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /**NavigationView*/
+        val navView: BottomNavigationView = binding.navView
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container_app) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        navView.setupWithNavController(navController)
+
+        binding.mHome.setOnClickListener {
+            navController.navigate(com.rkhvstnv.testecommerce.home.R.id.navigation_home)
+        }
     }
 }
