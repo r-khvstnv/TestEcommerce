e75c94556b81994a84038600ca2988f4d3574955
r-khvstnv <r.khvastunov@gmail.com>
2022-11-10 00:46:12 +0500

    Created UI part for DetailsScreen and ShopFragment

--- details/src/main/java/com/rkhvstnv/testecommerce/details/DetailsFragment.kt
+++ details/src/main/java/com/rkhvstnv/testecommerce/details/DetailsFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rkhvstnv.testecommerce.details.databinding.FragmentDetailsBinding
 
 class DetailsFragment : Fragment() {
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+    ): View {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = DetailsTabPagerAdapter(this, 3)
+        binding.viewPagerTabs.adapter = adapter
+    }
+
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
     }
 
 }
---
+++ details/src/main/java/com/rkhvstnv/testecommerce/details/DetailsTabPagerAdapter.kt
@@ -0,0 +1,10 @@
+package com.rkhvstnv.testecommerce.details
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class DetailsTabPagerAdapter(fragment: DetailsFragment, private val tabCount: Int): FragmentStateAdapter(fragment) {
+    override fun getItemCount(): Int = tabCount
+
+    override fun createFragment(position: Int): Fragment = ShopFragment()
+}
